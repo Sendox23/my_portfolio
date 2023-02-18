893f29d47fd683477dd2d551fe4d7e40eccae9e7
sendox23 <sendox@protonmail.com>
2023-02-18 13:55:23 -0800

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import { Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./Components/Layout/Root";
 import ErrorPage from "./pages/ErrorPage";
-import LoadingSpinner from "./Components/UI/LoadingSpinner";
+
 import AboutMePage from "./pages/AboutMePage";
 import ContactMePage from "./pages/ContactMePage";
 import LandingPage from "./pages/LandingPage";
 import WorkPage from "./pages/WorkPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      { path: "home", element: <LandingPage /> },
+      { path: "my-work", element: <WorkPage /> },
+      { path: "about-me", element: <AboutMePage /> },
+      { path: "contact-me", element: <ContactMePage /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Suspense fallback={<LoadingSpinner />}>
-          <Routes>
-            <Route path="/home" element={<LandingPage />} />
-            <Route path="/my-work" element={<WorkPage />} />
-            <Route path="/about-me" element={<AboutMePage />} />
-            <Route path="/contact-me" element={<ContactMePage />} />
-            <Route path="/" element={<LandingPage />} />
-            <Route element={<LandingPage />} />
-            <Route path="/*" element={<ErrorPage />} />
-          </Routes>
-        </Suspense>
-      </Layout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
