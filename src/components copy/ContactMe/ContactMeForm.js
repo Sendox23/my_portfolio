3e4b5ca0a2057169c6c7b07da3d7305097efceae
sendox23 <sendox@protonmail.com>
2023-02-27 13:50:02 -0800

    test

---
+++ src/components copy/ContactMe/ContactMeForm.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+import classes from "./ContactMeForm.module.css";
+
+const ContactMeForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // add your form submission logic here, for example, sending an HTTP request
+    console.log(formData);
+  };
+
+  return (
+    <div className={classes.contactMeForm}>
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message:
+            <textarea
+              id="message"
+              name="message"
+              rows="5"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactMeForm;
---
+++ src/components copy/MainNavigation/MainNavigation.js
@@ -0,0 +1,109 @@
+import { NavLink } from "react-router-dom";
+import { Fragment, useState, useEffect } from "react";
+import logo from "../../resources/logo.png";
+import classes from "./MainNavigation.module.css";
+import NavFooter from "./NavFooter";
+
+const MainNavigation = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    if (window.innerWidth >= 800) {
+      setIsNavOpen(true);
+    } else {
+      setIsNavOpen(false);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth >= 800) {
+      setIsNavOpen(true);
+    } else {
+      setIsNavOpen(false);
+    }
+  }, [windowWidth]);
+
+  const toggleNavMenuHandler = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <Fragment>
+      <nav
+        style={{
+          transform: isNavOpen ? "translateX(0)" : "translateX(-100%)",
+        }}
+      >
+        <NavLink to="/home">
+          <div className={classes.logoBox}>
+            <img className={classes.logo} src={logo} alt="logo" />
+          </div>
+        </NavLink>
+        <hr className={classes.dividerTop} />
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.link
+          }
+          to="/home"
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.link
+          }
+          to="/my-work"
+        >
+          My Work
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.link
+          }
+          to="/about-me"
+        >
+          About Me
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.link
+          }
+          to="/my-skills"
+        >
+          My Skills
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? classes.activeLink : classes.link
+          }
+          to="/contact-me"
+        >
+          Contact Me
+        </NavLink>
+        <hr className={classes.dividerBottom} />
+        <NavFooter />
+      </nav>
+      {windowWidth < 800 && (
+        <button className={classes.navToggle} onClick={toggleNavMenuHandler}>
+          ☰
+        </button>
+      )}
+    </Fragment>
+  );
+};
+
+export default MainNavigation;
