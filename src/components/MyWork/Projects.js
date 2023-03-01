642059bd10a0c7bfc422033eb9df9718ba9e806d
sendox23 <sendox@protonmail.com>
2023-02-28 16:37:49 -0800

    Updated css and added more test projects to my work section. Added carousel feature to images on my work section

--- src/components/MyWork/Projects.js
+++ src/components/MyWork/Projects.js
@@ -1,22 +1,58 @@
+import reactMeetup from "../../resources/react-meetups-project.png";
+import reactMeetup2 from "../../resources/react-meetups-project-2.png";
+
+import classes from "./Projects.module.css";
+
+
 const Projects = () => {
   const PROJECT_LIST = [
-    { name: "Spotify Playlist", id: "p1",  description: "", img: "" },
-    { name: "Fantasy Football", id: "p2",  description: "", img: "" },
-    { name: "Food Order App", id: "p3",  description: "", img: "" },
+    {
+      name: "Spotify Playlist",
+      id: "p1",
+      description: "",
+      img: [],
+      gitLink: "",
+    },
+    {
+      name: "Fantasy Football",
+      id: "p2",
+      description: "",
+      img: [],
+      gitLink: "",
+    },
+    { name: "Food Order App", id: "p3", description: "", img: [], gitLink: "" },
+    {
+      name: "React Meetups",
+      id: "p4",
+      description: "",
+      img: [reactMeetup, reactMeetup2],
+      gitLink: "https://github.com/Sendox23/React-Meetups",
+      websiteLink: "https://react-meetups-project5.netlify.app/",
+    },
   ];
-  const mappedList = PROJECT_LIST.map((project) => {
+  const mappedList = PROJECT_LIST.map((project, index) => {
     return (
-      
-        <li id={project.id} key={project.id}>
-          <h3>{project.name}</h3>
-          <p>{project.description}</p>
-          <img src={project.img} alt={project.name} />
-        </li>
-   
+      <li id={project.id} key={index}>
+        <h3>{project.name}</h3>
+        <p>{project.description}</p>
+        <div className={classes.carousel}>
+          {project.img.map((image, imgIndex) => (
+            <img
+              key={imgIndex}
+              src={image}
+              alt={`${project.name} ${imgIndex}`}
+            />
+          ))}
+        </div>
+      </li>
     );
   });
 
-  return <ul>{mappedList}</ul>;
+  return (
+    <div className={classes.myWork}>
+      <ul>{mappedList}</ul>
+    </div>
+  );
 };
 
 export default Projects;
