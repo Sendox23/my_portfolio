e19f1b5d7a7142c90c1db2e73c2896e1879b06ab
sendox23 <sendox@protonmail.com>
2023-03-01 14:12:05 -0800

    optimized Projects component and moved my projects to another file to clean up code

--- src/components/MyWork/Projects.js
+++ src/components/MyWork/Projects.js
@@ -1,118 +1,97 @@
-import reactMeetup from "../../resources/react-meetups-project.png";
-import reactMeetup2 from "../../resources/react-meetups-project-2.png";
-
-import classes from "./Projects.module.css";
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react"; // import useState, useMemo, and useCallback
+import { projectList } from "./project_list"; // import the projectList array
+import classes from "./Projects.module.css"; // import the CSS module
 
 const Projects = () => {
-  const [modalImage, setModalImage] = useState(null);
+  const [modalImage, setModalImage] = useState(null); // use state to keep track of the modal image
 
-  const openModal = (image) => {
+  const openModal = useCallback((image) => { // memoize the openModal function using useCallback
     setModalImage(image);
-  };
+  }, []);
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => { // memoize the closeModal function using useCallback
     setModalImage(null);
-  };
-  const scrollLeft = (e, carousel) => {
+  }, []);
+
+  const scrollLeft = useCallback((e, carousel) => { // memoize the scrollLeft function using useCallback
     e.stopPropagation();
     carousel.scrollBy({
       left: -carousel.offsetWidth,
       behavior: "smooth",
     });
-  };
-  
-  const scrollRight = (e, carousel) => {
+  }, []);
+
+  const scrollRight = useCallback((e, carousel) => { // memoize the scrollRight function using useCallback
     e.stopPropagation();
     carousel.scrollBy({
       left: carousel.offsetWidth,
       behavior: "smooth",
     });
-  };
-  const PROJECT_LIST = [
-    {
-      name: "Spotify Playlist",
-      id: "p1",
-      description: "",
-      img: [],
-      gitLink: "",
-      websiteLink: "",
-    },
-    {
-      name: "Fantasy Football",
-      id: "p2",
-      description: "",
-      img: [],
-      gitLink: "",
-      websiteLink: "",
-    },
-    {
-      name: "Food Order App",
-      id: "p3",
-      description: "",
-      img: [],
-      gitLink: "",
-      websiteLink: "",
-    },
-    {
-      name: "React Meetups",
-      id: "p4",
-      description: "",
-      img: [reactMeetup, reactMeetup2],
-      gitLink: "https://github.com/Sendox23/React-Meetups",
-      websiteLink: "https://react-meetups-project5.netlify.app/",
-    },
-  ];
-  const mappedList = PROJECT_LIST.map((project, index) => {
-    return (
-      <li id={project.id} key={index}>
-        <h3>{project.name}</h3>
-        <p>{project.description}</p>
-        <div className={classes.projectLinks}>
-          {project.gitLink && (
-            <a href={project.gitLink} target="_blank" rel="noreferrer">
-              GitHub
-            </a>
-          )}
-          {project.websiteLink && (
-            <a href={project.websiteLink} target="_blank" rel="noreferrer">
-              Link
-            </a>
-          )}
-        </div>
-        <div className={classes.carouselContainer}>
-          <button
-            className={`${classes.carouselButton} ${classes.carouselButtonLeft}`}
-            onClick={(e) => scrollLeft(e, e.target.parentNode.querySelector(`.${classes.carousel}`))}
-          >
-            &#8249;
-          </button>
-          <div className={classes.carousel}>
-            {project.img.map((image, imgIndex) => (
-              <img
-                key={imgIndex}
-                src={image}
-                alt={`${project.name} ${imgIndex}`}
-                onClick={() => openModal(image)}
-              />
-            ))}
-          </div>
-          <button
-            className={`${classes.carouselButton} ${classes.carouselButtonRight}`}
-            onClick={(e) => scrollRight(e, e.target.parentNode.querySelector(`.${classes.carousel}`))}
-          >
-            &#8250;
-          </button>
-        </div>
-      </li>
-    );
-  });
+  }, []);
+
+  const mappedList = useMemo(() => { // memoize the mappedList variable using useMemo
+    return projectList.map((project) => { // map over the projectList array to create a list of projects
+        const { id, name, description, img, gitLink, websiteLink } = project; // use object destructuring to simplify code
+        return (
+          <li id={id} key={id}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <div className={classes.projectLinks}>
+              {gitLink && (
+                <a href={gitLink} target="_blank" rel="noreferrer">
+                  GitHub
+                </a>
+              )}
+              {websiteLink && (
+                <a href={websiteLink} target="_blank" rel="noreferrer">
+                  Link
+                </a>
+              )}
+            </div>
+            <div className={classes.carouselContainer}>
+              <button
+                className={`${classes.carouselButton} ${classes.carouselButtonLeft}`}
+                onClick={(e) =>
+                  scrollLeft(
+                    e,
+                    e.target.parentNode.querySelector(`.${classes.carousel}`)
+                  )
+                }
+              >
+                &#8249;
+              </button>
+              <div className={classes.carousel}>
+                {img.map((image, imgIndex) => (
+                  <img
+                    key={imgIndex}
+                    src={image}
+                    alt={`${name} ${imgIndex}`}
+                    onClick={() => openModal(image)}
+                  />
+                ))}
+              </div>
+              <button
+                className={`${classes.carouselButton} ${classes.carouselButtonRight}`}
+                onClick={(e) =>
+                  scrollRight(
+                    e,
+                    e.target.parentNode.querySelector(`.${classes.carousel}`)
+                  )
+                }
+              >
+                &#8250;
+              </button>
+            </div>
+          </li>
+        );
+      });
+    }, [ openModal, scrollLeft, scrollRight]);
 
   return (
     <div className={classes.myWork}>
       <h2>My Work</h2>
-      <ul>{mappedList}</ul>
-      {modalImage && (
+      <ul>{mappedList}</ul> {/* Render the list of projects */}
+      {modalImage && ( // conditionally render the modal if modalImage is truthy
         <div className={classes.modal} onClick={closeModal}>
           <button className={classes.closeButton} onClick={closeModal}>
             X
---
+++ src/components/MyWork/project_list.js
@@ -0,0 +1,37 @@
+import reactMeetup from "../../resources/react-meetups-project.png";
+import reactMeetup2 from "../../resources/react-meetups-project-2.png";
+
+export const projectList = [
+    {
+      name: "Spotify Playlist",
+      id: "p1",
+      description: "",
+      img: [],
+      gitLink: "",
+      websiteLink: "",
+    },
+    {
+      name: "Fantasy Football",
+      id: "p2",
+      description: "",
+      img: [],
+      gitLink: "",
+      websiteLink: "",
+    },
+    {
+      name: "Food Order App",
+      id: "p3",
+      description: "",
+      img: [],
+      gitLink: "",
+      websiteLink: "",
+    },
+    {
+      name: "React Meetups",
+      id: "p4",
+      description: "",
+      img: [reactMeetup, reactMeetup2],
+      gitLink: "https://github.com/Sendox23/React-Meetups",
+      websiteLink: "https://react-meetups-project5.netlify.app/",
+    },
+  ];
