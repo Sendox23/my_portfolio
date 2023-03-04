b8f27ee96588503e1f4f2565f32553bef2e2117a
sendox23 <sendox@protonmail.com>
2023-03-03 19:25:38 -0800

    fixed spelling error

--- src/components/ContactMe/ContactMeForm.js
+++ src/components/ContactMe/ContactMeForm.js
@@ -20,12 +20,11 @@ const ContactMeForm = () => {
   function encode(data) {
     return Object.keys(data)
       .map(
-        (key) =>
-          encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
       )
       .join("&");
   }
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     fetch("/", {
@@ -43,54 +42,53 @@ const ContactMeForm = () => {
   return (
     <div className={classes.contactMeForm}>
       <h2>Contact</h2>
-      <form onSubmit={handleSubmit} name="form-name" method="POST" data-netlifty="true" >
-      <input type="hidden" name="form-name" value="contact" />
-        <div>
-          <label htmlFor="name">
-            Name:
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="email">
-            Email:
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="message">
-            Message:
-            <textarea
-              id="message"
-              name="message"
-              rows="5"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
+      <form
+        onSubmit={handleSubmit}
+        name="form-name"
+        method="POST"
+        data-netlify="true"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+
+        <label htmlFor="name">
+          Name:
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label htmlFor="message">
+          Message:
+          <textarea
+            id="message"
+            name="message"
+            rows="5"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
         <button type="submit">Submit</button>
       </form>
-
-
-
     </div>
-    
   );
 };
 
