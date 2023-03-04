55982354ea5c131c1e5a80a77aa549333614749a
sendox23 <sendox@protonmail.com>
2023-03-03 19:49:45 -0800

    test

--- src/components/ContactMe/ContactMeForm.js
+++ src/components/ContactMe/ContactMeForm.js
@@ -1,92 +1,46 @@
-import React, { useState } from "react";
-import { redirect } from "react-router-dom";
-
 import classes from "./ContactMeForm.module.css";
 
 const ContactMeForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": event.target.getAttribute("name"),
-        ...formData,
-      }),
-    })
-      .then(() => redirect("/home"))
-      .catch((error) => alert(error));
-  };
-
   return (
     <div className={classes.contactMeForm}>
       <h2>Contact</h2>
-      <form
-        onSubmit={handleSubmit}
-        name="form-name"
-        method="POST"
-        data-netlify="true"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-
-        <label htmlFor="name">
-          Name:
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </label>
-
-        <label htmlFor="email">
-          Email:
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </label>
-
-        <label htmlFor="message">
-          Message:
-          <textarea
-            id="message"
-            name="message"
-            rows="5"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </label>
-
-        <button type="submit">Submit</button>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label htmlFor="name">
+            Name:
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="message">
+            Message:
+            <textarea
+              id="message"
+              name="message"
+              rows="5"
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Submit</button>
+        </p>
       </form>
     </div>
   );
